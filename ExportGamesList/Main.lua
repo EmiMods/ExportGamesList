f5342545d99ecbc2b9d27dc5b335049b908bd95d
EmiMods <emily9716s@outlook.com>
2024-08-31 14:44:50 -0500

    Modified dec to hex formatting

--- ExportGamesList/Main.lua
+++ ExportGamesList/Main.lua
@@ -54,7 +54,7 @@ function showCancel()
 end
 
 function decToHex(decimal)
-    return string.format("%x", decimal)
+    return string.format(string.upper("%08x"), decimal)
 end
 
 function promptDriveSelect()
